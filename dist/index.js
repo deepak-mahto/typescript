ccd9dc138bf27e06a08d18d6080942f8afa3be8d
Deepak Kumar <mahtodeepak857@gmail.com>
2024-04-08 01:32:44 +0530

    Arrays and Tuples

--- dist/index.js
+++ dist/index.js
@@ -4,3 +4,15 @@ let id = 5;
 let company = "Apple";
 let isPublished = true;
 let x = "any type";
+// Arrays and Tuples
+// Arrays
+let ids = [1, 2, 3, 4, 5, 6, 7];
+let arr = [1, true, "hello"];
+// Tuple
+let person = [12, "deepak", true];
+// Tuples Array
+let employee = [
+    [1, "deepak"],
+    [2, "wiston"],
+    [3, "chuck"],
+];
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,19 @@ let id: number = 5;
 let company: string = "Apple";
 let isPublished: boolean = true;
 let x: any = "any type";
+
+// Arrays and Tuples
+
+// Arrays
+let ids: number[] = [1, 2, 3, 4, 5, 6, 7];
+let arr: any[] = [1, true, "hello"];
+
+// Tuple
+let person: [number, string, boolean] = [12, "deepak", true];
+
+// Tuples Array
+let employee: [number, string][] = [
+  [1, "deepak"],
+  [2, "wiston"],
+  [3, "chuck"],
+];
